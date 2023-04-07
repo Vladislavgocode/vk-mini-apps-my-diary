5072cbb688eb70527acf43fb035f0e3f32ef75bb
Vladislav <98208268+Vladislavgocode@users.noreply.github.com>
2023-04-07 14:08:28 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  transpileDependencies: true,
+  plugins: [
+    // basicSsl(),
+    vue(),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 3000,
+    // https: true,
+    proxy: {
+      '/api': {
+        target: 'http://188.120.233.52:5001',
+        changeOrigin: true
+      },
+    }
+    
+  },
+})
